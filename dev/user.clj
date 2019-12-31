b8b617b2378425881b59e89d3d2c964515972988
Daniel Compton <desk@danielcompton.net>
2020-01-01 11:09:39 +1300

    Remove some unneeded files

--- dev/user.clj
+++
@@ -1,9 +0,0 @@
-(ns user
-  (:require [org.clojurists-together.site.core :refer [config new-system]]
-            [reloaded.repl :refer [system init start stop go reset reset-all]]))
-
-(reloaded.repl/set-init! #(new-system :dev))
-
-(defn reset-and-test []
-  ; TODO: tests
-  (reset))
